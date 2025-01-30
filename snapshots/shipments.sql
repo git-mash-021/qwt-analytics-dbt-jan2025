6db83efaeee4eff99305fb3049fdcfc847e1e4e0
Ashish Mishra <ashish.mishra9@cognizant.com>
2025-01-30 11:50:02 +0000

    commited changes for Transforming schema and Snapshot

--- snapshots/shipments.sql
+++ snapshots/shipments.sql
@@ -2,8 +2,8 @@
     {{
         config
         (
-            target_database='QWT_ANALYTICS',
-            target_schema='SNAPSHOTS',
+            target_database=env_var('DBT_DATABASE', 'QWT_ANALYTICS'),
+            target_schema=env_var('DBT_SNAPSHOTSCHEMA', 'SNAPSHOTS'),
             unique_key="orderid||'-'||lineno",
             strategy='timestamp',
             updated_at='shipmentdate'
